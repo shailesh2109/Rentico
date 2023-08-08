79e31a3667706575a140d88f10fbbb806c72f7f4
shailesh2109 <74820911+shailesh2109@users.noreply.github.com>
2023-08-08 17:49:55 +0530

    completed make_booking file

--- lib/screens/make_booking.dart
+++ lib/screens/make_booking.dart
@@ -39,7 +39,27 @@ class _MakeBookingState extends State<MakeBooking> {
               
             ),
             SizedBox(height: 10,),
-            "Customer Name*".text.make(),
+            "Mobile Number*".text.make(),
+            SizedBox(height: 12,),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
+              decoration: 
+              InputDecoration(border: OutlineInputBorder(),
+              hintText: "+91 XX XX XX XX XX"),
+              validator:(value) {
+                if(value==null||value.isEmpty){
+                  return "Enter Customer Name";
+                }
+                if(value.length!=10){
+                  return"Enter valid Mobile Number";
+                }
+                return null;
+              },
+              
+            ),
+            SizedBox(height: 10,),
+            "Nationality*".text.make(),
             SizedBox(height: 12,),
             TextFormField(
               decoration: 
@@ -54,30 +74,15 @@ class _MakeBookingState extends State<MakeBooking> {
               
             ),
             SizedBox(height: 10,),
-            "Customer Name*".text.make(),
+            "License Number*".text.make(),
             SizedBox(height: 12,),
             TextFormField(
               decoration: 
               InputDecoration(border: OutlineInputBorder(),
-              hintText: "Melon Tusk"),
+              hintText: "UP XXXX XXXX XXXXX"),
               validator:(value) {
                 if(value==null||value.isEmpty){
-                  return "Enter Customer Name";
-                }
-                return null;
-              },
-              
-            ),
-            SizedBox(height: 10,),
-            "Customer Name*".text.make(),
-            SizedBox(height: 12,),
-            TextFormField(
-              decoration: 
-              InputDecoration(border: OutlineInputBorder(),
-              hintText: "Melon Tusk"),
-              validator:(value) {
-                if(value==null||value.isEmpty){
-                  return "Enter Customer Name";
+                  return "Enter valid no.";
                 }
                 return null;
               },
@@ -89,21 +94,21 @@ class _MakeBookingState extends State<MakeBooking> {
         
           ),
       )),
-    Step(
-      state: currentStep>0?StepState.complete:StepState.indexed,
+      Step(
+      state: currentStep>1?StepState.complete:StepState.indexed,
       title: Text("Step 2"),
       isActive: currentStep>=1,
       content: Form(
-        key: formkeys[0],
+        key: formkeys[1],
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            "Customer Name*".text.make(),
+            "Adhar Card No*".text.make(),
             SizedBox(height: 12,),
             TextFormField(
               decoration: 
               InputDecoration(border: OutlineInputBorder(),
-              hintText: "Melon Tusk"),
+              hintText: "XXXX XXXX XXXX"),
               validator:(value) {
                 if(value==null||value.isEmpty){
                   return "Enter Customer Name";
@@ -113,48 +118,65 @@ class _MakeBookingState extends State<MakeBooking> {
               
             ),
             SizedBox(height: 10,),
-            "Customer Name*".text.make(),
+            "Voter ID No.".text.make(),
             SizedBox(height: 12,),
             TextFormField(
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
               decoration: 
               InputDecoration(border: OutlineInputBorder(),
-              hintText: "Melon Tusk"),
-              validator:(value) {
-                if(value==null||value.isEmpty){
-                  return "Enter Customer Name";
-                }
-                return null;
-              },
+              hintText: "XXXX XXXX XXXX"),
+              
               
             ),
             SizedBox(height: 10,),
-            "Customer Name*".text.make(),
+            "Passport No.".text.make(),
             SizedBox(height: 12,),
             TextFormField(
               decoration: 
               InputDecoration(border: OutlineInputBorder(),
-              hintText: "Melon Tusk"),
-              validator:(value) {
-                if(value==null||value.isEmpty){
-                  return "Enter Customer Name";
-                }
-                return null;
-              },
+              hintText: "XXXX XXXX XXXX"),
+              
               
             ),
             SizedBox(height: 10,),
-            "Customer Name*".text.make(),
+            "Vissa No.".text.make(),
             SizedBox(height: 12,),
             TextFormField(
               decoration: 
               InputDecoration(border: OutlineInputBorder(),
-              hintText: "Melon Tusk"),
-              validator:(value) {
-                if(value==null||value.isEmpty){
-                  return "Enter Customer Name";
-                }
-                return null;
-              },
+              hintText: "XXXX XXXX XXXXX"),
+              
+              
+            ),
+            SizedBox(height: 10,),
+            "Hotel Name".text.make(),
+            SizedBox(height: 12,),
+            TextFormField(
+              decoration: 
+              InputDecoration(border: OutlineInputBorder(),
+              hintText: "Radisson Hotel"),
+             
+              
+            ),
+            SizedBox(height: 10,),
+            "Room No.".text.make(),
+            SizedBox(height: 12,),
+            TextFormField(
+              decoration: 
+              InputDecoration(border: OutlineInputBorder(),
+              hintText: "509"),
+             
+              
+            ),
+            SizedBox(height: 10,),
+            "Bike/Scooty No.".text.make(),
+            SizedBox(height: 12,),
+            TextFormField(
+              decoration: 
+              InputDecoration(border: OutlineInputBorder(),
+              hintText: "XXXX XXXX XXXXX"),
+              
               
             ),
             SizedBox(height: 10,),
@@ -163,23 +185,53 @@ class _MakeBookingState extends State<MakeBooking> {
         
           ),
       )),
-    Step(title: Text("Step 3"), content: Container()),
+      Step(
+            state: currentStep>2? StepState.complete:StepState.indexed,
+            isActive: currentStep>=2,
+            title: const Text('Step 3'),
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                
+                "We have sent a verification code to*".text.xl.make(),
+                SizedBox(height: 12,),
+                Form(
+                  key: formkeys[2],
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
+                    validator: (value) {
+                      if(value==null||value.isEmpty){
+                        return "Enter the verification code";
+                      }
+                     
+                    },
+                    
+                  ),
+                ),
+                SizedBox(height: 12,)
+              ],
+              
+            ),
+          ),
+    
+   
 
   ];
+ int currentStep=0;
   bool complete =false;
-  int currentStep=0;
   next(){
-    if(formkeys[currentStep].currentState!.validate()){
-      currentStep+1!=getSteps().length
-      ?goTo(currentStep+1)
-      : setState(() => complete=true,);
+    if( formkeys[currentStep].currentState!.validate()){
+    currentStep +1!= getSteps().length
+    ? goTo(currentStep+1)
+    : setState(() => complete=true);
     }
-
   }
+  
   goTo(int step){
-    setState(() {
-      currentStep=step;
-    });
+    
+    setState(()=>currentStep=step);
+    
   }
   @override
   Widget build(BuildContext context) {
@@ -195,16 +247,16 @@ class _MakeBookingState extends State<MakeBooking> {
        child:
        Column(
         children: [
-          complete?
-          Expanded(child: Container(
+          complete
+            ?Expanded(child: Container(
               child: Column(
                 
                 children: [
                   SizedBox(height: 200,),
-                  Container(child: Image.asset("assets//images//Completed.png")).w(1000).h(100),
-                  "Received Successful".text.xl2.make(),
-                  "You have successfully received a".text.make(),
-                   "vehicle at Rentico".text.make(),
+                  Container(child: Image.asset("assets/images/Completed.png")).w(1000).h(100),
+                  "Booking Successful".text.xl2.make(),
+                  "You have successfully made a ".text.make(),
+                   "booking at Rentico".text.make(),
                    SizedBox(height: 10,),
                    ElevatedButton( 
                     onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context)=>homeScreen())), 
@@ -217,6 +269,7 @@ class _MakeBookingState extends State<MakeBooking> {
               steps: getSteps(),
               type: StepperType.horizontal,
               onStepContinue: next,
+              currentStep: currentStep,
               controlsBuilder:(context, details) {
                 return Container(
                   child: ElevatedButton(onPressed: details.onStepContinue, child: "Next".text.make()),
